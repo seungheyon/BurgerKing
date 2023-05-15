4526bb1f69c4057e4f196f839f60ef472d0dfb15
ãhanseunghee < zzuggumee@gmail.com>
2023-05-15 15:47:47 +0900

    Refactor : 프로젝트 내 변수명 카멜케이스로 변경

--- src/main/java/com/example/burgerking/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/burgerking/security/UserDetailsServiceImpl.java
@@ -16,10 +16,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username)
+        User user = userRepository.findByUserName(username)
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
 
-        return new UserDetailsImpl(user, user.getUsername());
+        return new UserDetailsImpl(user, user.getUserName());
     }
 
 }
