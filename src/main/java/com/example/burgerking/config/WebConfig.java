86c3b29f9d5050fb3ce5e9cf297a6457cc133100
JH4 <spainclub@naver.com>
2023-05-15 16:51:11 +0900

    Feat : CORS 추가

---
+++ src/main/java/com/example/burgerking/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.example.burgerking.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"); // 허용할 HTTP method
+    }
+}
--- src/main/java/com/example/burgerking/controller/UserController.java
+++ src/main/java/com/example/burgerking/controller/UserController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
