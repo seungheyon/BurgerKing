2db6f3c026172d283ff9e497dc86e25090b50e4a
JH4 <spainclub@naver.com>
2023-05-16 15:08:59 +0900

    Feat : 메뉴 전체조회 추가

--- src/main/java/com/example/burgerking/repository/MenuRepository.java
+++ src/main/java/com/example/burgerking/repository/MenuRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
     List<Menu> findByCategoryOrderByCreatedDateDesc(String category);
+    List<Menu> findAllByOrderByCreatedDateDesc();
 }
